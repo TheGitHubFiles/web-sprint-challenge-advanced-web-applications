2ff46627b978740501b2c08c7ed2d82f9e194914
Andrew Poppenberg <andrewpoppenberg@yahoo.com>
2020-11-13 10:30:45 -0600

    finished my login page

---
+++ client/src/utils/axiosWithAuth.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export function axiosWithAuth() {
+    const token = localStorage.getItem("token");
+    return axios.create({
+        baseURL: "http://localhost:5000",
+        headers: {
+            Authorization: token,
+        },
+    });
+}
